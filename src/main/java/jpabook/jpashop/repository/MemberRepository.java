d96d4120df873d2452b2115d349a8c65d85ff8b5
YoonMin <rladbsals23@gmail.com>
2021-04-19 22:17:06 +0900

    feat : API로직 추가
    RestController(MemberApiController.java)를 추가하여 Postman으로 JSON데이터 확인

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -26,7 +26,7 @@ public class MemberRepository {
     }
 
     public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class)
+        return em.createQuery("select m from Member m join fetch m.orders", Member.class)
                 .getResultList();
     }
 
