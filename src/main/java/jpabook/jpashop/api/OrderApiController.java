949ef83f430c1742b4c25922a285e21aa97050e3
YoonMin <rladbsals23@gmail.com>
2021-04-22 23:53:40 +0900

    feat : 컬렉션 조회 최적화 v3, v3.1
    [v3]
    distinct를 통해 실제 DB쿼리에서 distinct쿼리를 날려줌
    하지만 본 예제에서는 orderItems에 의해 order가 배로 증가함
    distinct가 선언된 경우 jpa가 중복된 엔티티를 제거해줌
    단점 : 페이징 처리 불가
    
    [v3.1]
    x To One 관계인것만 fetch조인하여 *페이징처리*
    나머지는 LAZY로딩을 하는데, default_batch_fetch_size옵션(글로벌)을 줘서
    in쿼리로 한번에 가져옴 글로벌로 설정하기 싫다면 컬렉션에 @BatchSize 사용

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -38,6 +39,37 @@ public class OrderApiController {
     @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 컬렉션 패치조인
+     * distinct : DB에 실제 distinct쿼리를 날려주고
+     * 가져온 데이터에서 중복인 엔티티를 제거해준다
+     * 단점 : 페이징 불가
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        return orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * xtoOne 관계인 것만 fetch join 후 *페이징처리*
+     * 나머지는 LAZY로딩하는데 default_batch_fetch_size 옵션을 줘서 in쿼리로 처리
+     * 위 옵션은 글로벌한 옵션이고, 각 엔티티마다 적용하고 싶으면 @BatchSize(size=...)사용
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value="offset", defaultValue = "0") int offset,
+                                        @RequestParam(value="limit", defaultValue = "100") int limit
+                                        ) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
         return orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
