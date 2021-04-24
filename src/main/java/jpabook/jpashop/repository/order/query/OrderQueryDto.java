235ce8b7e38b8d0df457c742848221c8a92dd9b9
YoonMin <rladbsals23@gmail.com>
2021-04-24 14:14:01 +0900

    feat : 컬렉션 조회 최적화 v4
    ToOne 관계는 한번에 조인해도 데이터 row수가 증가하지 않기 때문에 join으로 가져옴
    
    ToMany 관계는 조인하면 데이터 row수가 증가하므로 ToOne관계인 데이터만 가져온 뒤 for문을 돌며 별도 메소드에서 조회

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -14,5 +14,14 @@ public class OrderQueryDto {
     private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
-    private List<OrderItemQueryDto> orderItemQueryDtoList;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        //this.orderItems = orderItems;
+    }
 }
